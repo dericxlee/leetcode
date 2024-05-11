d5facab63ede7639fff7a73e74c267a7787efe61
Deric Lee <dericleee@gmail.com>
2024-05-10 23:12:20 -0700

    Time: 80 ms (10.95%), Space: 54.6 MB (63.05%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    let max = 0, left = 0, right = 0, k = 1;
+
+    while(right < nums.length){
+        if(nums[right] === 0) k--
+        if(k < 0){
+            if(nums[left] === 0) k++
+            left++
+        }
+        right++
+    };
+
+    return right - left - 1;
+};
