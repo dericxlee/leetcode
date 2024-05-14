9d79ea762b317dcd3ee66343ab6b9a554456656e
Deric Lee <dericleee@gmail.com>
2024-05-13 21:10:31 -0700

    Time: 43 ms (93.28%), Space: 48.5 MB (81.53%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let alt = 0;
+    let max = 0;
+
+    for(let i = 0; i < gain.length; i++){
+        alt += gain[i];
+
+        if(alt > max) max = alt;
+    };
+
+    return max;
+};
