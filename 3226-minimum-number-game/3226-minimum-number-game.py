33df514189eb79e1ba06e0fb88e8ac74e2870cc4
Deric Lee <dericleee@gmail.com>
2024-09-06 21:31:05 -0700

    Time: 50 ms (71.95%), Space: 16.7 MB (35.27%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        heapq.heapify(nums)
+        res = []
+
+        while nums:
+            a = heapq.heappop(nums)
+            b = heapq.heappop(nums)
+
+            res.append(b)
+            res.append(a)
+
+        return res
