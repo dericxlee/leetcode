b9d4412745cb33525aafe72517824b4ad5338880
Deric Lee <dericleee@gmail.com>
2024-09-06 22:05:08 -0700

    Time: 77 ms (25.58%), Space: 18 MB (14.64%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,25 +1,20 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
         heap = []
-        counter = 1
 
         for i in range(len(score)):
             heapq.heappush(heap, (-score[i], i))
         
-        while heap:
-            scores, idx = heapq.heappop(heap)
+        for rank in range(len(score)):
+            score_value, idx = heapq.heappop(heap)
 
-            if counter == 1:
+            if rank == 0:
                 score[idx] = "Gold Medal"
-            elif counter == 2:
+            elif rank == 1:
                 score[idx] = 'Silver Medal'
-            elif counter == 3:
+            elif rank == 2:
                 score[idx] = 'Bronze Medal'
             else:
-                score[idx] = str(counter)
-            
-            counter+=1
+                score[idx] = str(rank + 1)
         
         return score
-
-
