112f3b851d5f2978f519e53891e5371f6e34c981
Deric Lee <dericleee@gmail.com>
2024-10-02 16:39:28 -0700

    Time: 441 ms (19.48%), Space: 70.3 MB (99.03%) - LeetHub

--- 2512-longest-uploaded-prefix/2512-longest-uploaded-prefix.py
+++ 2512-longest-uploaded-prefix/2512-longest-uploaded-prefix.py
@@ -2,24 +2,19 @@ class LUPrefix:
 
     def __init__(self, n: int):
         self.min_heap = []
-        self.max_heap = []
+        self.longest_prefix = 0
 
     def upload(self, video: int) -> None:
-        if video == len(self.max_heap) + 1:
-            heapq.heappush(self.max_heap, -video)
+        if video == self.longest_prefix + 1:
+            self.longest_prefix = video
         else:
             heapq.heappush(self.min_heap, video)
 
-        while self.min_heap and self.min_heap[0] == len(self.max_heap) + 1:
-            video = heapq.heappop(self.min_heap)
-            heapq.heappush(self.max_heap, - video)
+        while self.min_heap and self.min_heap[0] == self.longest_prefix + 1:
+            self.longest_prefix = heapq.heappop(self.min_heap)
         
-
     def longest(self) -> int:
-        if self.max_heap:
-            return abs(self.max_heap[0])
-        
-        return 0
+        return self.longest_prefix
 
 
 # Your LUPrefix object will be instantiated and called as such:
