1c94bed4138b5d00082e28b5002bf9fe25f05170
Deric Lee <dericleee@gmail.com>
2024-10-15 19:08:17 -0700

    Time: 38 ms (73.66%), Space: 16.8 MB (82.5%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+        
+        visited = {}
+
+        queue = deque([node])
+        visited[node] = Node(node.val, [])
+
+        while queue:
+            curr = queue.popleft()
+
+            for neighbor in curr.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, []) 
+                    queue.append(neighbor)
+                
+                visited[curr].neighbors.append(visited[neighbor])
+        
+        return visited[node]
+        
+
+
+
