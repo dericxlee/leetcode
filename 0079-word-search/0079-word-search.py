a94124acd39aecd103bd9c0e895bc28716b7f9d6
Deric Lee <dericleee@gmail.com>
2024-11-05 19:11:30 -0800

    Time: 4227 ms (48.33%), Space: 16.6 MB (84.05%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,38 +3,35 @@ class Solution:
         row = len(board)
         col = len(board[0])
 
-        def dfs(i, j, index, memo):
-            if len(memo) - 1 > len(word) or i < 0 or j < 0 or i >= row or j >= col or board[i][j] != word[index] or (i, j) in memo:
-                return False
-            
-            if index + 1 == len(word):
+        def backtrack(m, n, index, memo):
+            if len(memo) == len(word):
                 return True
 
-            memo.add((i, j))
-            index+=1
+            if m < 0 or n < 0 or m >= row or n >= col or board[m][n] != word[index] or (m, n) in memo:
+                return False
+
+            memo.add((m, n))
             
             directions = (
-                dfs(i+1, j, index, memo) or
-                dfs(i-1, j, index, memo) or
-                dfs(i, j+1, index, memo) or
-                dfs(i, j-1, index, memo)
+                backtrack(m+1, n, index+1, memo) or
+                backtrack(m-1, n, index+1, memo) or
+                backtrack(m, n+1, index+1, memo) or
+                backtrack(m, n-1, index+1, memo)
             )
 
-            memo.remove((i, j))
-
+            memo.remove((m, n))
             return directions
         
         for i in range(row):
             for j in range(col):
-                if dfs(i, j, 0, set()):
+                if backtrack(i, j, 0, set()):
                     return True
         
         return False
         
-        
-        
 
             
 
+            
 
 
