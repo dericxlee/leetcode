688d0b2e5b88a4a13b6b7c600295c9ab5717d51b
Deric Lee <dericleee@gmail.com>
2024-11-13 18:49:30 -0800

    Time: 0 ms (100%), Space: 25 MB (8.33%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def solve(start, end):
+            if start > end:
+                return -1
+            
+            mid = (start + end) // 2
+
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] > target:
+                return solve(start, mid - 1)
+            if nums[mid] < target:
+                return solve(mid + 1, end)
+            
+        return solve(0, len(nums) - 1)
