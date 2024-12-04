884ec3ac4754650aa59a48bad2efc241b6a8c769
Deric Lee <dericleee@gmail.com>
2024-12-03 17:41:26 -0800

    Time: 57 ms (90.97%), Space: 17.7 MB (31.28%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,16 +1,17 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        m, n = len(str1), len(str2)
-        left = right = 0
-        print(ord('z'), ord('a'))
+        pointer = 0
+        n = len(str2)
 
-        while left < m and right < n:
-            s1 = str1[left]
-            s2 = str2[right]
+        for char in str1:
+            if pointer >= n:
+                break
 
-            if ord(s1) == ord(s2) or (ord(s1) - 96) % 26 == ord(s2) - 97:
-                right+=1
-            
-            left+=1
+            num1 = ord(char)
+            num2 = ord(str2[pointer])
+
+            if num1 == num2 or (num1 - 96) % 26 == num2 - 97:
+                pointer += 1
         
-        return right == n
+        return n == pointer
+
