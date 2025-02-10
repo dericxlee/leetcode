05eab9cccdc3b601f8c06af6153b8f51b555787c
Deric Lee <dericleee@gmail.com>
2025-02-10 15:34:09 -0800

    Time: 8 ms (39.86%), Space: 18.8 MB (92.09%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,31 +1,27 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        row = len(isConnected)
-        graph = {i: [] for i in range(row)}
+        n = len(isConnected)
         visited = set()
         provinces = 0
+        queue = deque()
 
-        for n in range(row):
-            for m in range(row):
-                if isConnected[n][m]:
-                    graph[n].append(m)
-
-        for i in range(row):
+        for i in range(n):
             if i not in visited:
+                visited.add(i)
+                queue.append(i)
                 provinces += 1
-                queue = deque([i])
 
                 while queue:
-                    node = queue.popleft()
-                    visited.add(node)
-
-                    for neighbor in graph[node]:
-                        if neighbor not in visited:
-                            queue.append(neighbor)
+                    city = queue.popleft()
+                    
+                    for j in range(n):
+                        if isConnected[city][j] == 1 and j not in visited:
+                            visited.add(j)
+                            queue.append(j)
 
         return provinces
-                     
-                    
+
+
 
 
 
