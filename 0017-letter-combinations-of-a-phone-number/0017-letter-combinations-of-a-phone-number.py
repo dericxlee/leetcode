39ca22f17bd854e2991b2248a34ff97eb5eafbb2
Deric Lee <dericleee@gmail.com>
2025-02-11 20:58:18 -0800

    Time: 0 ms (100%), Space: 17.9 MB (22.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,29 +2,29 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-            
+
         phone = {
-            '2': ['a', 'b', 'c'], 
-            '3': ['d', 'e', 'f'], 
-            '4': ['g', 'h', 'i'], 
-            '5': ['j', 'k', 'l'], 
-            '6': ['m', 'n', 'o'], 
-            '7': ['p', 'q', 'r', 's'], 
-            '8': ['t', 'u', 'v'], 
-            '9': ['w', 'x', 'y', 'z']
-            }
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
 
         result = []
 
-        def dfs(i, combo):
-            if len(combo) == len(digits):
+        def backtrack(i, combo):
+            if i >= len(digits):
                 result.append(combo)
                 return
             
             for char in phone[digits[i]]:
-                dfs(i+1, combo+char)
-            
-        dfs(0, '')
+                backtrack(i+1, combo+char)
+        
+        backtrack(0, "")
         return result
-
+            
 
