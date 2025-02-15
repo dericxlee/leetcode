1b761bd06984f8eb21907e5c886ecdec38764034
Deric Lee <dericleee@gmail.com>
2025-02-14 18:49:56 -0800

    Time: 3 ms (28.51%), Space: 17.7 MB (48.88%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -1,6 +1,6 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        digits = "0123456789"
+        digits = set(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
         stack = []
 
         for char in s:
