f2b3435d79d925c74cdfe1f2361e8713b3c914cd
Deric Lee <dericleee@gmail.com>
2025-03-05 18:57:41 -0800

    Time: 5 ms (50.3%), Space: 17.6 MB (92.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,22 +6,16 @@ class Solution:
 
         for i in range(9):
             for j in range(9):
-                current = board[i][j]
-
-                if current == '.':
+                if board[i][j] == ".":
                     continue
 
-                if current in row[i]:
-                    return False
+                num = board[i][j]
 
-                if current in col[j]:
+                if num in row[i] or num in col[j] or num in sq[(i // 3, j // 3)]:
                     return False
                 
-                if current in sq[i//3, j//3]:
-                    return False
-
-                row[i].add(current)
-                col[j].add(current)
-                sq[i//3, j//3].add(current)
+                row[i].add(num)
+                col[j].add(num)
+                sq[i // 3, j // 3].add(num)
         
         return True
