5dcc3a7e2f4a1764d6d87ca7d79945366ed428d6
Deric Lee <dericleee@gmail.com>
2025-04-16 17:36:50 -0700

    Time: 8 ms (88.17%), Space: 17.9 MB (32.33%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -4,11 +4,10 @@ class Solution:
         result = 0
 
         for i, num in enumerate(nums):
-            if index[num]:
-                for idx in index[num]:
-                    prod = i * idx
-                    if prod % k == 0:
-                        result += 1
+            for idx in index[num]:
+                prod = i * idx
+                if prod % k == 0:
+                    result += 1
             
             index[num].append(i)
         
