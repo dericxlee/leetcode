591ca2d7909fdbb4d5a2bb035a9fb56c31a899fa
Deric Lee <dericleee@gmail.com>
2026-04-28 17:34:50 -0700

    Time: 0 ms (100%), Space: 20.6 MB (73.27%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,27 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = list()
-        operators = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            '/': operator.truediv,
-        }
+        stack = []
+        operators = "+-*/"
 
         for token in tokens:
-            if token in operators:
-                y = stack.pop()
-                x = stack.pop()
-
-                num = int(operators[token](x, y))
-                stack.append(num)
-            else:
+            if token not in operators:
                 stack.append(int(token))
+            else:
+                second = stack.pop()
+                first = stack.pop()
+
+                if token == "+":
+                    sub = first + second
+                elif token == "-":
+                    sub = first - second
+                elif token == "*":
+                    sub = first * second
+                elif token == "/":
+                    sub = abs(first) // abs(second)
+                    if (first < 0 or second < 0) and not (first < 0 and second < 0):
+                        sub *= (-1)
+
+                stack.append(sub)
         
         return stack[-1]
-
-
+                
